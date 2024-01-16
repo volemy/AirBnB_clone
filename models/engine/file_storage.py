aa76d0b03eac41b6d67bf22a2a5311581b1394ca
volemy <minayobenevolence0@gmail.com>
2024-01-16 22:06:15 +0000

    fileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ This filestorage class serializes instances to a JSON format.
 """
 
 import json
+import shlex
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -30,9 +31,23 @@ class FileStorage():
             "Review": Review
         }
 
-    def all(self):
-        """ This method returns the dictionary of all objects """
-        return self.__objects
+    def all(self, cls=None):
+        """ This method returns a dictionary
+        Return:
+            list of objects of one type of class
+        """
+        list_objects = {}
+
+        if cls:
+            for key, value in self.__objects.items():
+                class_name, obj_id = key.split('.')
+                if class_name == __cls.name__:
+                    list_objects[key] = value
+        else:
+            list_objects = self.__objects
+
+        return list_objects
+
 
     def new(self, obj):
         """ This method adds a new object to the __objects dictionary """
@@ -63,3 +78,14 @@ class FileStorage():
                     self.__objects[key] = obj
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """
+        This method deletes obj froem __objects if its inside and
+        does not do anything if obj equals to None
+        """
+        if obj:
+            key = "{}".{}.format(type(obj).__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
+                self.save()
